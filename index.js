3bdce8216fa020e777597da24ff0b584c162107b
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-08 18:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ console.log('HELLO'.toLowerCase());
 
 function sayHiToGrandma(string) {
 if (string.toLowerCase() === string) {
-return 'I cant hear you!';
+return "I can't hear you!";
 } if (string.toUpperCase() === string) {
 return "YES INDEED!";
 } if (string === `I love you, Grandma.` ) {
