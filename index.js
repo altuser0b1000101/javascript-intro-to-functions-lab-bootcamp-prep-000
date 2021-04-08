55150127bb4c756d5a63a6559780c07ffc347ada
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-08 18:11:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,21 +16,5 @@ console.log('HELLO'.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-var lowercase = 'hello!'
 
-'HELLO'.toLowerCase() === lowercase
-
-var uppercase = "HELLO!"
- 
-uppercase.toUpperCase() === uppercase // true
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
-
-}
 
