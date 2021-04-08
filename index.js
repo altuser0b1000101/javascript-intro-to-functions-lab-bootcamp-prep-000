7a216ff1f0936257defd192dcd45719666a590d3
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-08 18:25:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,11 +15,3 @@ function logWhisper(string) {
 console.log('HELLO'.toLowerCase());
 }
 
-
-function sayHiToGrandma(string) {
-  var lowercase = 'hello!'
-  lowercase.toLowerCase() === lowercase
-  if sayHiToGrandma === lowercase
-  return string 
-  
-}
