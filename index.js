f12044f98e814d52433c4f5c22a54e8589e47100
Franklin Sahlhoff <franklinsahlhoff@gmail.com>
2021-04-08 18:33:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,20 +16,10 @@ console.log('HELLO'.toLowerCase());
 }
 
 function sayHiToGrandma(string) {
-
-var uppercase = 'YES INDEED!'
- 
-uppercase.toUpperCase() === uppercase // true
- 
-var lowercase = 'I cant hear you!'
- 
-lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'I love you, too.'
- 
-mixedCase.toLowerCase() === mixedCase // false
- 
-mixedCase.toUpperCase() === mixedCase // false
-
-return string
-}
+if (string.toLowerCase() === string) {
+return “I can’t hear you!” ;
+} if (string.toUpperCase() === string ) {
+return “YES INDEED!”
+} if (string === `I love you, Grandma.` ) {
+return “I love you, too.”
+}}
